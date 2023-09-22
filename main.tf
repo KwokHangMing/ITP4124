7d1348ddde6d2aaec8b196207534e0ce1279e9ed
Leo Kwok <112677134+KwokHangMing@users.noreply.github.com>
2023-09-22 12:50:58 +0000

    modified code.

--- main.tf
+++ main.tf
@@ -50,14 +50,6 @@ resource "azurerm_storage_account" "staticweb" {
   }
 }
 
-
-
-# resource "azurerm_storage_container" "staticweb" {
-#   name                  = "$web"
-#   storage_account_name  = azurerm_storage_account.staticweb.name
-#   container_access_type = "blob"
-# }
-
 resource "azurerm_storage_blob" "staticweb_index" {
   name                   = "index.html"
   storage_account_name   = azurerm_storage_account.staticweb.name
@@ -98,62 +90,17 @@ resource "azurerm_virtual_network" "azfunction_network" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_storage_account.azfunction.location
   resource_group_name = azurerm_resource_group.rg.name
-}
 
-resource "azurerm_subnet" "azfunction_subnet1" {
-  name                 = "azfunction_subnet1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.azfunction_network.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "example-delegation"
-
-    service_delegation {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
+  subnet {
+    name           = "projVnet1Prod_subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+  subnet {
+    name           = "projVnet1Prod_subnet2"
+    address_prefix = "10.0.0.0/24"
   }
 }
 
-resource "azurerm_subnet" "azfunction_subnet2" {
-  name                 = "azfunction_subnet2"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.azfunction_network.name
-  address_prefixes     = ["10.0.0.0/24"]
-
-  delegation {
-    name = "example-delegation"
-
-    service_delegation {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
-}
-
-# resource "azurerm_app_service_plan" "example" {
-#   name                = "example-app-service-plan"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-
-#   sku {
-#     tier = "Standard"
-#     size = "S1"
-#   }
-# }
-
-# resource "azurerm_app_service" "example" {
-#   name                = "example-app-service"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-#   app_service_plan_id = azurerm_app_service_plan.example.id
-# }
-
-# resource "azurerm_app_service_virtual_network_swift_connection" "example" {
-#   app_service_id = azurerm_app_service.example.id
-#   subnet_id      = azurerm_subnet.example.id
-# }
 
 #static web virtual network
 resource "azurerm_virtual_network" "staticweb_network" {
@@ -161,37 +108,15 @@ resource "azurerm_virtual_network" "staticweb_network" {
   address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-}
 
-resource "azurerm_subnet" "staticweb_subnet1" {
-  name                 = "staticweb_subnet1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.staticweb_network.name
-  address_prefixes     = ["10.1.1.0/24"]
-
-  delegation {
-    name = "example-delegation"
-
-    service_delegation {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
+  subnet {
+    name           = "projVnet2Prod_subnet1"
+    address_prefix = "10.1.1.0/24"
   }
-}
 
-resource "azurerm_subnet" "staticweb_subnet2" {
-  name                 = "staticweb_subnet2"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.staticweb_network.name
-  address_prefixes     = ["10.1.0.0/24"]
-
-  delegation {
-    name = "example-delegation"
-
-    service_delegation {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
+  subnet {
+    name           = "projVnet2Prod_subnet2"
+    address_prefix = "10.1.0.0/24"
   }
 }
 
